02a5458d9dd17329318de76ba87148b8b2f497a8
Akanbi Dire <direakanbi@icloud.com>
2022-02-03 22:45:22 +0100

    Fixed Linter Errors

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import RocketItem from './RocketItems';
 import { fetchRocketApi } from '../redux/rockets/rockets';
 import '../styling/rocket.css';
+
 const Rockets = () => {
   const rockets = useSelector((state) => state.rocketsReducer);
   const dispatch = useDispatch();
